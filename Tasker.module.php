890a620c6b68527652a307f31024e1d83bf08071
mtwebit <mt+github@webit.hu>
2018-06-12 11:29:04 +0200

    a new todo

--- Tasker.module.php
+++ Tasker.module.php
@@ -559,6 +559,7 @@ class Tasker extends WireData implements Module {
       $res = false;
       $this->message($e->getMessage());
       $this->message(ob_get_contents());
+      // TODO loggig?
       // $this->log->save('json', $e->getMessage());
       // $this->log->save('json', ob_get_contents());
       ob_end_clean();
